3efeec7059f2037203aedc23d0ddd2023f83c4a6
packedcrack <52038722+PackedCrack@users.noreply.github.com>
2026-01-26 12:26:09 +0100

    Removed local account discovery - its not part of t1082.
    Removed drive discovery - its not part of t1082.

--- src/T1082/T1082.c
+++ src/T1082/T1082.c
@@ -10,9 +10,6 @@
 #include <Windows.h>
 #include <lm.h>
 
-// TODO: REMOVE ME
-#pragma comment(lib, "Netapi32.lib")
-
 
 static DWORD get_registry_dword_value(LPWSTR pValue)
 {
@@ -89,131 +86,131 @@ static size_t get_hostname(LPWSTR pOut, size_t outSize)
 
 	return wcslen(pOut);
 }
-static LPWSTR get_privilege(DWORD priv)
-{
-	if (priv == USER_PRIV_GUEST)
-	{
-		return L"Guest";
-	}
-	else if (priv == USER_PRIV_USER)
-	{
-		return L"User";
-	}
-	else if (priv == USER_PRIV_ADMIN)
-	{
-		return L"Administrator";
-	}
-
-	return L"Unknown Privilege";
-}
-static void log_local_accounts(HANDLE hLog)
-{
-	LPBYTE* pBuffer = NULL;
-	DWORD entriesRead = 0;
-	DWORD totalEntires = 0;
-	DWORD resume = 0;
-
-	while (TRUE)
-	{
-		DWORD result = NetUserEnum(NULL,
-								   1,
-								   FILTER_NORMAL_ACCOUNT,
-								   &pBuffer,
-								   MAX_PREFERRED_LENGTH,
-								   &entriesRead,
-								   &totalEntires,
-								   &resume);
-
-		if (result == ERROR_ACCESS_DENIED)
-		{
-			write_to_file(hLog, L"NetUserEnum failed with: ERROR_ACCESS_DENIED\n");
-			break;
-		}
-
-		if (result == NERR_Success || result == ERROR_MORE_DATA)
-		{
-			USER_INFO_1* pInfo = (USER_INFO_1*) pBuffer;
-			for (DWORD i = 0; i < entriesRead; ++i)
-			{
-				WCHAR line[2048] = { 0 };
-				LPWSTR privilege = get_privilege(pInfo->usri1_priv);
-				swprintf(line, 
-						 ARRAYSIZE(line), 
-						 L"\nAccount Name: %ls\nPrivilege: %ls\nComment: %ls\n",
-						 pInfo->usri1_name,
-						 privilege,
-						 pInfo->usri1_comment);
-
-				write_to_file(hLog, line);
-
-				if (pInfo->usri1_password != NULL)
-				{
-					WCHAR password[128] = { 0 };
-					swprintf(password,
-							 ARRAYSIZE(password),
-							 L"Password: %ls\nPassword Age: %d",
-							 pInfo->usri1_password,
-							 pInfo->usri1_password_age);
-					write_to_file(hLog, password);
-				}
-
-				if (pInfo->usri1_home_dir != NULL)
-				{
-					if (wcslen(pInfo->usri1_home_dir) > 0)
-					{
-						WCHAR home[128] = { 0 };
-						swprintf(home,
-								 ARRAYSIZE(home),
-								 L"Home Directory: %ls\n",
-								 pInfo->usri1_home_dir);
-						write_to_file(hLog, home);
-					}
-				}
-
-				if (pInfo->usri1_script_path != NULL)
-				{
-					if (wcslen(pInfo->usri1_script_path) > 0)
-					{
-						WCHAR script[128] = { 0 };
-						swprintf(script,
-								 ARRAYSIZE(script),
-								 L"Script Path: %ls\n",
-								 pInfo->usri1_script_path);
-						write_to_file(hLog, script);
-					}
-				}
-
-				DWORD flags = pInfo->usri1_flags;
-				if (flags & UF_ACCOUNTDISABLE)
-				{
-					write_to_file(hLog, L"Account Disabled\n");
-				}
-				if (flags & UF_PASSWD_NOTREQD)
-				{
-					write_to_file(hLog, L"No Password Required\n");
-				}
-				if (flags & UF_DONT_REQUIRE_PREAUTH)
-				{
-					write_to_file(hLog, L"Kerberos Not Required\n");
-				}
-
-				++pInfo;
-			}
-
-
-			if (pBuffer)
-			{ 
-				NetApiBufferFree(pBuffer);
-				pInfo = NULL;
-			}
-
-			if (result == NERR_Success)
-			{
-				break;
-			}
-		}
-	}
-}
+//static LPWSTR get_privilege(DWORD priv)
+//{
+//	if (priv == USER_PRIV_GUEST)
+//	{
+//		return L"Guest";
+//	}
+//	else if (priv == USER_PRIV_USER)
+//	{
+//		return L"User";
+//	}
+//	else if (priv == USER_PRIV_ADMIN)
+//	{
+//		return L"Administrator";
+//	}
+//
+//	return L"Unknown Privilege";
+//}
+//static void log_local_accounts(HANDLE hLog)
+//{
+//	LPBYTE* pBuffer = NULL;
+//	DWORD entriesRead = 0;
+//	DWORD totalEntires = 0;
+//	DWORD resume = 0;
+//
+//	while (TRUE)
+//	{
+//		DWORD result = NetUserEnum(NULL,
+//								   1,
+//								   FILTER_NORMAL_ACCOUNT,
+//								   &pBuffer,
+//								   MAX_PREFERRED_LENGTH,
+//								   &entriesRead,
+//								   &totalEntires,
+//								   &resume);
+//
+//		if (result == ERROR_ACCESS_DENIED)
+//		{
+//			write_to_file(hLog, L"NetUserEnum failed with: ERROR_ACCESS_DENIED\n");
+//			break;
+//		}
+//
+//		if (result == NERR_Success || result == ERROR_MORE_DATA)
+//		{
+//			USER_INFO_1* pInfo = (USER_INFO_1*) pBuffer;
+//			for (DWORD i = 0; i < entriesRead; ++i)
+//			{
+//				WCHAR line[2048] = { 0 };
+//				LPWSTR privilege = get_privilege(pInfo->usri1_priv);
+//				swprintf(line, 
+//						 ARRAYSIZE(line), 
+//						 L"\nAccount Name: %ls\nPrivilege: %ls\nComment: %ls\n",
+//						 pInfo->usri1_name,
+//						 privilege,
+//						 pInfo->usri1_comment);
+//
+//				write_to_file(hLog, line);
+//
+//				if (pInfo->usri1_password != NULL)
+//				{
+//					WCHAR password[128] = { 0 };
+//					swprintf(password,
+//							 ARRAYSIZE(password),
+//							 L"Password: %ls\nPassword Age: %d",
+//							 pInfo->usri1_password,
+//							 pInfo->usri1_password_age);
+//					write_to_file(hLog, password);
+//				}
+//
+//				if (pInfo->usri1_home_dir != NULL)
+//				{
+//					if (wcslen(pInfo->usri1_home_dir) > 0)
+//					{
+//						WCHAR home[128] = { 0 };
+//						swprintf(home,
+//								 ARRAYSIZE(home),
+//								 L"Home Directory: %ls\n",
+//								 pInfo->usri1_home_dir);
+//						write_to_file(hLog, home);
+//					}
+//				}
+//
+//				if (pInfo->usri1_script_path != NULL)
+//				{
+//					if (wcslen(pInfo->usri1_script_path) > 0)
+//					{
+//						WCHAR script[128] = { 0 };
+//						swprintf(script,
+//								 ARRAYSIZE(script),
+//								 L"Script Path: %ls\n",
+//								 pInfo->usri1_script_path);
+//						write_to_file(hLog, script);
+//					}
+//				}
+//
+//				DWORD flags = pInfo->usri1_flags;
+//				if (flags & UF_ACCOUNTDISABLE)
+//				{
+//					write_to_file(hLog, L"Account Disabled\n");
+//				}
+//				if (flags & UF_PASSWD_NOTREQD)
+//				{
+//					write_to_file(hLog, L"No Password Required\n");
+//				}
+//				if (flags & UF_DONT_REQUIRE_PREAUTH)
+//				{
+//					write_to_file(hLog, L"Kerberos Not Required\n");
+//				}
+//
+//				++pInfo;
+//			}
+//
+//
+//			if (pBuffer)
+//			{ 
+//				NetApiBufferFree(pBuffer);
+//				pInfo = NULL;
+//			}
+//
+//			if (result == NERR_Success)
+//			{
+//				break;
+//			}
+//		}
+//	}
+//}
 static void collect_hostname_account_info(HANDLE hLog)
 {
 	assert(hLog != INVALID_HANDLE_VALUE);
@@ -228,7 +225,8 @@ static void collect_hostname_account_info(HANDLE hLog)
 			 hostname);
 	write_to_file(hLog, line);
 
-	log_local_accounts(hLog);
+	// Not part of T1082
+	//log_local_accounts(hLog);
 }
 static void log_cpu_and_memory(HANDLE hLog)
 {
@@ -269,89 +267,90 @@ static void log_cpu_and_memory(HANDLE hLog)
 		write_to_file(hLog, memoryLine);
 	}
 }
-static void log_hard_drives(HANDLE hLog)
-{
-	WCHAR volume[MAX_PATH] = { 0 };
-	HANDLE hVolume = FindFirstVolumeW(volume, ARRAYSIZE(volume));
-	if (hVolume == INVALID_HANDLE_VALUE)
-	{
-		PRINT_WIN32_ERROR(FindFirstVolumeW);
-		assert(FALSE);
-		return;
-	}
-
-
-	while (TRUE)
-	{
-		DWORD size = 0;
-		BOOL success = GetVolumePathNamesForVolumeNameW(volume, NULL, 0, &size);
-		if (!success)
-		{
-			DWORD err = GetLastError();
-			if (err != ERROR_MORE_DATA)
-			{
-				printf("GetVolumePathNamesForVolumeNameW failed with: 0x%X", err);
-				assert(FALSE);
-				break;
-			}
-		}
-
-		Vector names = VECTOR_CREATE(WCHAR, size);
-		assert(VECTOR_SIZE(names) == size);
-
-		success = GetVolumePathNamesForVolumeNameW(volume, names.pData, VECTOR_SIZE(names), &size);
-		if (!success)
-		{
-			PRINT_WIN32_ERROR(GetVolumePathNamesForVolumeNameW);
-			assert(FALSE);
-			break;
-		}
-
-		if (names.pData != L'\0')
-		{
-			WCHAR line[512] = { 0 };
-			swprintf(line, ARRAYSIZE(line), L"Volume: %ls\n", volume);
-			write_to_file(hLog, line);
-
-			const WCHAR* pName = names.pData;
-			while (*pName != L'\0')
-			{
-				WCHAR mountPoint[512] = { 0 };
-				swprintf(mountPoint, ARRAYSIZE(mountPoint), L"MountPoint: %ls\n", pName);
-				write_to_file(hLog, mountPoint);
-
-				pName += wcslen(pName) + 1;
-			}
-		}
-
-		VECTOR_DESTROY(names);
-
-		if (!FindNextVolumeW(hVolume, volume, ARRAYSIZE(volume)))
-		{
-			DWORD err = GetLastError();
-			if (err != ERROR_NO_MORE_FILES)
-			{
-				printf("FindNextVolumeW failed with: 0x%X", err);
-				assert(FALSE);
-			}
-
-			break;
-		}
-	}
-	
-	if (!FindVolumeClose(hVolume))
-	{
-		PRINT_WIN32_ERROR(FindVolumeClose);
-		assert(FALSE);
-	}
-}
+//static void log_hard_drives(HANDLE hLog)
+//{
+//	WCHAR volume[MAX_PATH] = { 0 };
+//	HANDLE hVolume = FindFirstVolumeW(volume, ARRAYSIZE(volume));
+//	if (hVolume == INVALID_HANDLE_VALUE)
+//	{
+//		PRINT_WIN32_ERROR(FindFirstVolumeW);
+//		assert(FALSE);
+//		return;
+//	}
+//
+//
+//	while (TRUE)
+//	{
+//		DWORD size = 0;
+//		BOOL success = GetVolumePathNamesForVolumeNameW(volume, NULL, 0, &size);
+//		if (!success)
+//		{
+//			DWORD err = GetLastError();
+//			if (err != ERROR_MORE_DATA)
+//			{
+//				printf("GetVolumePathNamesForVolumeNameW failed with: 0x%X", err);
+//				assert(FALSE);
+//				break;
+//			}
+//		}
+//
+//		Vector names = VECTOR_CREATE(WCHAR, size);
+//		assert(VECTOR_SIZE(names) == size);
+//
+//		success = GetVolumePathNamesForVolumeNameW(volume, names.pData, VECTOR_SIZE(names), &size);
+//		if (!success)
+//		{
+//			PRINT_WIN32_ERROR(GetVolumePathNamesForVolumeNameW);
+//			assert(FALSE);
+//			break;
+//		}
+//
+//		if (names.pData != L'\0')
+//		{
+//			WCHAR line[512] = { 0 };
+//			swprintf(line, ARRAYSIZE(line), L"Volume: %ls\n", volume);
+//			write_to_file(hLog, line);
+//
+//			const WCHAR* pName = names.pData;
+//			while (*pName != L'\0')
+//			{
+//				WCHAR mountPoint[512] = { 0 };
+//				swprintf(mountPoint, ARRAYSIZE(mountPoint), L"MountPoint: %ls\n", pName);
+//				write_to_file(hLog, mountPoint);
+//
+//				pName += wcslen(pName) + 1;
+//			}
+//		}
+//
+//		VECTOR_DESTROY(names);
+//
+//		if (!FindNextVolumeW(hVolume, volume, ARRAYSIZE(volume)))
+//		{
+//			DWORD err = GetLastError();
+//			if (err != ERROR_NO_MORE_FILES)
+//			{
+//				printf("FindNextVolumeW failed with: 0x%X", err);
+//				assert(FALSE);
+//			}
+//
+//			break;
+//		}
+//	}
+//	
+//	if (!FindVolumeClose(hVolume))
+//	{
+//		PRINT_WIN32_ERROR(FindVolumeClose);
+//		assert(FALSE);
+//	}
+//}
 static void collect_hardware_info(HANDLE hLog)
 {
 	write_to_file(hLog, L"\n\nHardware Info\n");
 
 	log_cpu_and_memory(hLog);
 
-	log_hard_drives(hLog);
+	// Not part of T1082
+	//log_hard_drives(hLog);
 }
 static Vector get_preferred_system_language()
 {
