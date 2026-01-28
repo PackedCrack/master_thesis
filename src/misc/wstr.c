bf3798e3db3d7d732832f0addd45b6eedb9d8d7a
packedcrack <52038722+PackedCrack@users.noreply.github.com>
2026-01-28 17:32:15 +0100

    Added WideString wrapper implementation.

---
+++ src/misc/wstr.c
@@ -0,0 +1,87 @@
+#include "wstr.h"
+
+#include <assert.h>
+#include <string.h>
+
+
+WideString details_wstring_create(const wchar_t* literal, size_t size)
+{
+    WideString s = { 0 };
+    s.array = VECTOR_CREATE(wchar_t, size);
+    s.length = size - 1;    // length does not count the null terminator
+
+    assert(s.array.pData != NULL);
+    memcpy(s.array.pData, literal, size * sizeof(wchar_t));
+
+    return s;
+}
+WideString details_wstring_create2(const wchar_t* pStr)
+{
+    WideString s = { 0 };
+    s.length = wcslen(pStr);
+    s.array = VECTOR_CREATE(wchar_t, s.length + 1);
+
+    assert(s.array.pData != NULL);
+
+    if (s.length)
+    {
+        memcpy(s.array.pData, pStr, s.length * sizeof(wchar_t));
+    }
+
+    wchar_t* pNull = (wchar_t*) s.array.pData;
+    pNull[s.length] = L'\0';
+
+    return s;
+}
+void details_wstring_destroy(WideString* pStr)
+{
+    VECTOR_DESTROY(pStr->array);
+    pStr->length = 0;
+}
+const wchar_t* details_wstring_c_str(const WideString* pStr)
+{
+    return (wchar_t*) pStr->array.pData;
+}
+size_t details_wstring_size(const WideString* pStr)
+{
+    return pStr->length;
+}
+wchar_t details_wstring_back(const WideString* pStr)
+{
+    assert(pStr->array.pData != NULL);
+
+    wchar_t* string = pStr->array.pData;
+    return string[pStr->length - 1];
+}
+void details_wstring_push_back(WideString* pStr, wchar_t c)
+{
+    assert(pStr->array.pData != NULL);
+
+    wchar_t newElement = L'\0';
+    VECTOR_PUSH_BACK(pStr->array, wchar_t, newElement);
+
+    wchar_t* string = pStr->array.pData;
+    string[pStr->length] = c;
+    pStr->length += 1;
+}
+WideString details_wstring_concatenate(WideString* pLhs, const wchar_t* pRhs)
+{
+    assert(pLhs->array.pData != NULL);
+    assert(pRhs != NULL);
+
+    WideString concatenated = { 0 };
+    concatenated.length = pLhs->length + wcslen(pRhs);
+    concatenated.array = VECTOR_CREATE(wchar_t, concatenated.length + 1);
+
+    wchar_t* pDst = (wchar_t*) concatenated.array.pData;
+    const wchar_t* pSrc = (wchar_t*) pLhs->array.pData;
+    memcpy(pDst, pSrc, pLhs->length * sizeof(wchar_t));
+    pDst += pLhs->length;
+    
+    memcpy(pDst, pRhs, wcslen(pRhs) * sizeof(wchar_t));
+
+    wchar_t* pElement = VECTOR_BACK(concatenated.array, wchar_t);
+    *pElement = L'\0';
+
+    return concatenated;
+}
---
+++ src/misc/wstr.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "vector.h"
+
+
+typedef struct
+{
+	Vector array;
+	size_t length;
+} WideString;
+
+WideString details_wstring_create(const wchar_t* literal, size_t size);
+#define WSTRING_CREATE(literal) details_wstring_create(literal, sizeof(literal) / sizeof(wchar_t))
+
+WideString details_wstring_create2(const wchar_t* pStr);
+#define WSTRING_CREATE_FROM_LPCWSTR(string) details_wstring_create2(string);
+
+void details_wstring_destroy(WideString* pStr);
+#define WSTRING_DESTROY(string) details_wstring_destroy(&string)
+
+const wchar_t* details_wstring_c_str(const WideString* pStr);
+#define WSTRING_C_STR(string) details_wstring_c_str(&string)
+
+size_t details_wstring_size(const WideString* pStr);
+#define WSTRING_SIZE(string) details_wstring_size(&string)
+
+wchar_t details_wstring_back(const WideString* pStr);
+#define WSTRING_BACK(string) details_wstring_back(&string)
+
+void details_wstring_push_back(WideString* pStr, wchar_t c);
+#define WSTRING_PUSH_BACK(string, c) details_wstring_push_back(&string, c);
+
+WideString details_wstring_concatenate(WideString* pLhs, const wchar_t* pRhs);
+#define WSTRING_CONCAT(string, LPCWSTR) details_wstring_concatenate(&string, LPCWSTR);
